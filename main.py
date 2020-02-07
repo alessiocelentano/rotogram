3aeaec7c85e39ec6cca8c9f96474651dadcebd2e
alessiocelentano <alessiocelentano2003@gmail.com>
2020-02-07 17:37:07 +0100

    Fix all bugs with abilities

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 # ---formal stuff---
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers = {'User-Agent': user_agent}
 base_url = 'https://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon)'
-url = base_url.format(input('Nome Pokémon: '))
+url = base_url.format(re.sub('[\s]', '_', input('Nome Pokemon: ').title()))
 request = urllib.request.Request(url, None, headers)
 response = urllib.request.urlopen(request)
 data = response.read()
@@ -18,36 +20,20 @@ for stat in stats:
     print(stat.text)
 
 # ABILITIES
-parents_list = soup.find_all('td', {'class': 'roundy', 'colspan': '2'})
-parent = ''
-for i in parents_list:
-    try:
-        if 'Abilities' in i.contents[1].text:
-            parent = i
-            break
-    except IndexError:
-        continue
-if not parent:  # 1 ability
-    # try again, but search "Ability"
-    parents_list = soup.find_all('td', {'class': 'roundy', 'colspan': '2'})
-    if parents_list == []:
-        parents_list = soup.find_all('td', {'class': 'roundy', 'width': '50%'})
-    for i in parents_list:
-        if 'Ability' in i.contents[1].text:
-            parent = i
-            break
-    abilities = parent.find_all('td', {'style': 'padding-top:3px; padding-bottom:3px'})
-    # for pkmn like Necrozma
-    abilities.append(parent.find_all('td', {'colspan': '10'})[0])
-else:
-    abilities = parent.find_all('td', {'style': ''})
-
-for ability in abilities:
-    try:  # 2+ abilities
-        form = ability.contents[3].text
-        ab = ability.contents[1].text
-        print(form + ': ' + ab)
-    except IndexError:  # 1/2 abilities
-        form = 'Ability'
-        ab = ability.contents[1].text
-        print(form + ': ' + ab)
+parents_list = soup.find_all('td', {'class': 'roundy'})
+for parent in parents_list:
+    if 'Ability' in parent.a.attrs.values():
+        par = parent
+        break
+abilities_list = par.find_all('td')
+index = 1
+for ability in abilities_list:
+    if 'display: none' not in ability.attrs.values():
+        for i in ability.find_all('a'):
+            ab = i.span.text
+            if ability.small is None:
+                form = 'Ability ' + str(index)
+                index += 1
+            else:
+                form = ability.small.text
+            print(form + ': ' + ab)
