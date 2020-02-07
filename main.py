8f33d7872a8477eb86f5711f28b8bfa186682ce5
alessiocelentano <alessiocelentano2003@gmail.com>
2020-02-07 18:56:34 +0100

    Fix all bugs with stats

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+import json
 
 # ---formal stuff---
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers = {'User-Agent': user_agent}
 base_url = 'https://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon)'
-url = base_url.format(re.sub('[\s]', '_', input('Nome Pokemon: ').title()))
+name = (re.sub('[\s]', '_', input('Nome Pokemon: ').title()))
+url = base_url.format(name)
 request = urllib.request.Request(url, None, headers)
 response = urllib.request.urlopen(request)
 data = response.read()
@@ -15,9 +17,27 @@ soup = BeautifulSoup(data, 'html.parser')
 # ---formal stuff---
 
 # STATS
-stats = soup.find_all('div', {'style': 'float:right'})
-for stat in stats:
-    print(stat.text)
+ref1 = soup.find('span', {'id': 'Base_stats'})
+zone1 = ref1.findAllNext('span', {'class': 'mw-headline'})
+ref2 = soup.find('span', {'class': 'mw-headline', 'id': 'Pok.C3.A9athlon_stats'})
+if not ref2:
+    ref2 = soup.find('span', {'class': 'mw-headline', 'id': 'Type_effectiveness'})
+zone2 = ref2.findAllPrevious('span', {'class': 'mw-headline'})
+forms = [elem for elem in zone1 if elem in zone2]
+stats = ['HP', 'Atk', 'Def', 'SpA', 'SpD', 'Spe', 'Total']
+if not forms:
+    forms = ['Base stats']
+values = soup.find_all('div', {'style': 'float:right'})
+index = 0
+for form in forms:
+    try:
+        print('\n' + form.text)
+    except AttributeError:
+        print('\n' + form)
+    for stat in stats:
+        print(stat + ': ' + values[index].text)
+        index += 1
+print('')
 
 # ABILITIES
 parents_list = soup.find_all('td', {'class': 'roundy'})
