349d0d645e34a0a18cc9a0839c77ee6670ec40bd
alessiocelentano <alessiocelentano2003@gmail.com>
2020-02-16 12:22:10 +0100

    Rearrange the code

--- json_generator.py
+++ json_generator.py
@@ -11,8 +11,7 @@ with open('pkmn.json', 'r') as filee:
     data = json.load(filee)
 
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers = {'User-Agent': user_agent}
+headers = {'User-Agent': 'Mozilla/5.0'}
 base_url = 'https://pokemondb.net/pokedex/{}'
 name = input('Insert Pokémon: ')
 url = base_url.format(re.sub(' ', '-', name))
@@ -24,85 +23,112 @@ dataa = response.read()
 soup = BeautifulSoup(dataa, 'html.parser')
 
 
-name = re.sub(' ', '_', name.lower())
-data[name] = {}
+# NAME
+pkmn = re.sub(' ', '_', name.lower())
+data[pkmn] = {}
 
 
 # FORMS
-forms_table = soup.find('div', {'class': 'tabs-tab-list'})
+forms_table = soup.find(
+    'div', {
+        'class': 'tabs-tab-list'
+    }
+)
 forms_list = [i.text for i in forms_table.find_all('a')]
 
 
 # DEX, TYPING, SPECIES, HEIGHT, WEIGHT AND ABILITIES
-forms_data = soup.find_all('div', {'class': 'grid-col span-md-6 span-lg-4'})
-del forms_data[0]
-for form_data, form in zip(forms_data, forms_list):
+pokedex_data_list = soup.find_all(
+    'div', {
+        'class': 'grid-col span-md-6 span-lg-4'
+    }
+)
+del pokedex_data_list[0]  # Ads space, no data
+
+for pokedex_data, form in zip(pokedex_data_list, forms_list):
     form = re.sub(' ', '_', form.lower())
-    data[name][form] = {}
-    keys_list = form_data.find_all('th')
+    data[pkmn][form] = {}
+    keys_list = pokedex_data.find_all('th')
     for key in keys_list:
         value = key.findNext('td')
-        key = re.sub(' ', '', key.text.lower())
-        key = re.sub('\u2116', '', key)
+        key = re.sub('[\u2116 ]', '', key.text.lower())
+
         if key == 'abilities':
-            data[name][form][key] = {}
-            index = 1
+            data[pkmn][form][key] = {}
+            index = 1  # Number the abilities
             if value.text != '—':  # For Partner Pikachu/Eevee
-                for ab in value.span.find_all('a'):
-                    data[name][form][key]['ability' + str(index)] = ab.text
+                for ability in value.span.find_all('a'):
+                    ability = ability.text
+                    data[pkmn][form][key]['ability' + str(index)] = ability
                     index += 1
                 if value.small:
-                    for ab in value.small.find('a'):
-                        data[name][form][key]['hidden_ability'] = value.small.a.text
-        elif key == 'local':
-            data[name][form][key] = {}
-            local_list = re.findall('[0-9][0-9][0-9]', value.text)
-            for game, local in zip(value.find_all('small'), local_list):
+                    for ability in value.small.find('a'):
+                        data[pkmn][form][key]['hidden_ability'] = ability
+                        # No increment because Hidden Abilities aren't numered
+
+        elif key == 'local':  # Dex number of each region
+            data[pkmn][form][key] = {}
+            dex_num_list = re.findall('[0-9][0-9][0-9]', value.text)
+            for game, num in zip(value.find_all('small'), dex_num_list):
                 game = re.sub('[^A-Z0-9]', '', game.text).lower()
                 if 'sm' in game or 'usum' in game:
-                    # For Alola Dex
                     game = game[:-1]
-                if 'xy' in game:
-                    # For Kalos Dex
+                    # Delete the "Alola dex" part
+                elif 'xy' in game:
                     game = 'xy'
+                    # Delete the part of Kalos
+                    # e.g. Central, Coustal and Mountain
                 elif game == 'lgplge':
-                    # LGPE is the correct abbreviation
                     game = 'lgpe'
                 elif game == 'p':
-                    # Pt is the correct abbreviation
                     game = 'pt'
                 elif game == 'ss':
-                    # SwSh is the correct abbreviation
                     game = 'swsh'
-                data[name][form][key][game] = local
+                data[name][form][key][game] = num
+
         elif key == 'type':
-            value = value.text[1:-1]
-            types = re.split(' ', value)
+            value = value.text[1:-1]  # Delete useless characters
+            types_list = re.split(' ', value)
             data[name][form][key] = {}
-            data[name][form][key]['type1'] = types[0]
-            if len(types) > 1:
-                data[name][form][key]['type2'] = types[1]
+            data[name][form][key]['type1'] = types_list[0]
+            if len(types_list) > 1:
+                data[name][form][key]['type2'] = types_list[1]
+
         elif key == 'height' or key == 'weight':
             values = re.split(' ', value.text)
             data[name][form][key] = {}
             data[name][form][key]['si'] = values[0]
             data[name][form][key]['usc'] = re.sub('[()]', '', values[1])
+
         else:
             data[name][form][key] = re.sub('\n', '', value.text)
 
 
 # STATS
-stats_list = soup.find_all('div', {'class': 'grid-col span-md-12 span-lg-8'})
+all_stats = soup.find_all(
+    'div', {
+        'class': 'grid-col span-md-12 span-lg-8'
+    }
+)
 key_list = ['hp', 'atk', 'def', 'spa', 'spd', 'spe']
 values_list = []
-total_list = []
-for stats in stats_list:
-    if stats == stats_list[-1]:
-        pass
-    else:
-        values_form = [stat.text for stat in stats.find_all('td', {'class': 'cell-num'})]
-        values_list.append(values_form)
-        total_list.append(stats.find('td', {'class': 'cell-total'}).text)
+total_list = []  # Sum of the 6 stats
+for stats in all_stats:
+    if stats != all_stats[-1]:  # The last one element is the position
+        stats_list = stats.find_all(
+            'td', {
+                'class': 'cell-num'
+            }
+        )
+        values = [value.text for value in stats_list]
+        values_list.append(values)
+        total_list.append(
+            stats.find(
+                'td', {
+                    'class': 'cell-total'
+                }
+            ).text
+        )
 for form in forms_list:
     form = re.sub(' ', '_', form).lower()
     data[name][form]['base_stats'] = {}
@@ -119,9 +145,13 @@ for form in forms_list:
 
 # EV yield, cath rate, base friendship, base exp and growth rate
 # Egg groups, gender and egg cycles
-data_list = soup.find_all('div', {'class': 'grid-col span-md-6 span-lg-12'})
-index = 0
-count = 0
+data_list = soup.find_all(
+    'div', {
+        'class': 'grid-col span-md-6 span-lg-12'
+    }
+)
+index = 0  # For iterate forms_list
+count = 0  # For increment index
 for i in data_list:
     if count == 2:
         index += 1
@@ -142,17 +172,32 @@ for i in data_list:
         elif key == 'gender':
             data[name][form][key] = value.text
         else:
-            data[name][form][key] = re.split(' ', value.text.replace('\n', ''))[0]
+            value = value.text.replace('\n', '')
+            data[name][form][key] = re.split(' ', value)[0]
     count += 1
 
 
 # Evolutions
 for form in forms_list:
     form = re.sub(' ', '_', form.lower())
-    data[name][form]['evo_methods'] = {'from': None, 'to': None}
-    evo_lines_list = soup.find_all('div', {'class': 'infocard-list-evo'})
-    split_lines_list = evo_lines_list[0].find_all('div', {'class': 'infocard-list-evo'})
-    evo_lines_list = [i for i in evo_lines_list if i not in split_lines_list]
+    data[name][form]['evo_methods'] = {
+        'from': None, 'into': None
+    }
+    evo_lines_list = soup.find_all(
+        'div', {
+            'class': 'infocard-list-evo'
+        }
+    )
+    split_evo_list = evo_lines_list[0].find_all(
+        'div', {
+            'class': 'infocard-list-evo'
+        }
+    )
+    # split_evo_list points the mons that have multiple evolutions
+    # Its elements are evo_lines_list children
+    # So to avoid useless iterations they will delete in the following line
+    evo_lines_list = [i for i in evo_lines_list if i not in split_evo_list]
+    # To avoid an unreadable code, Eevee is separated by the other mons
     eevee = ['eevee', 'partner_eevee']
     eeveelutions = [
         'flareon', 'vaporeon', 'jolteon',
@@ -172,9 +217,14 @@ for form in forms_list:
             else:
                 family = []
             for pkmn in pkmns:
-                infos = pkmn.find('span', {'class': 'infocard-lg-data text-muted'})
+                infos = pkmn.find(
+                    'span', {
+                        'class': 'infocard-lg-data text-muted'
+                    }
+                )
                 infos_list = infos.find_all('small')
                 if len(infos_list) == 3:
+                    # infos_list length is 3 if the mon have differents form
                     pkmn_name = infos_list[1]
                     pkmn_name = re.sub(' ', '_', pkmn_name.text.lower())
                 else:
@@ -182,21 +232,45 @@ for form in forms_list:
                     pkmn_name = re.sub(' ', '_', pkmn_name.lower())
                 if pkmn_name not in family:
                     family.append(pkmn_name)
-            evo_split = line.find_all('span', {'class': 'infocard-evo-split'})
-            evo_methods = line.find_all('span', {'class': 'infocard infocard-arrow'})
+            evo_split = line.find_all(
+                'span', {
+                    'class': 'infocard-evo-split'
+                }
+            )
+            evo_methods = line.find_all(
+                'span', {
+                    'class': 'infocard infocard-arrow'
+                }
+            )
             evo_methods = [i for i in evo_methods if i not in evo_split]
             for evo_method in evo_methods:
                 method_text = re.sub('[()]', '', evo_method.small.text)
-                fromm = evo_method.findPrevious('div', {'class': 'infocard'})
-                fromm = fromm.find('a', {'class': 'ent-name'}).text
+                fromm = evo_method.findNext(
+                    'div', {
+                        'class': 'infocard'
+                    }
+                )
+                fromm = fromm.find(
+                    'a', {
+                        'class': 'ent-name'
+                    }
+                ).text
                 fromm = re.sub(' ', '_', fromm.lower())
-                to = evo_method.findNext('div', {'class': 'infocard'})
-                to = to.find('a', {'class': 'ent-name'}).text
-                to = re.sub(' ', '_', to.lower())
+                into = evo_method.findPrevious(
+                    'div', {
+                        'class': 'infocard'
+                    }
+                )
+                into = into.find(
+                    'a', {
+                        'class': 'ent-name'
+                    }
+                ).text
+                into = re.sub(' ', '_', into.lower())
                 if form == fromm or ('partner' in form and fromm in form):
                     data[name][form]['evo_methods']['from'] = method_text
-                if form == to or ('partner' in form and to in form):
-                    data[name][form]['evo_methods']['to'] = method_text
+                if form == into or ('partner' in form and into in form):
+                    data[name][form]['evo_methods']['into'] = method_text
             if form not in eevee:
                 if form in family:
                     break
@@ -206,7 +280,7 @@ for form in forms_list:
         preevo = True
         if form == 'partner_eevee':
             family[0] = form
-        if form == 'partner_pikachu':
+        elif form == 'partner_pikachu':
             family[1] = form
         for i in family:
             if form == i:
