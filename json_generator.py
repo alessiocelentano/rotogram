d899561eb32d1b6fe401d732057045b2a5f63685
alessiocelentano <alessiocelentano@outlook.com>
2020-03-02 11:56:51 +0100

    Add "Pokémon changes" as general feature

--- json_generator.py
+++ json_generator.py
@@ -384,10 +384,10 @@ changes_list = soup.find_all('h3')
 changes_list = [i for i in changes_list if 'changes' in i.text]
 for form, changes in zip(form_list, changes_list):
 	form = re.sub(' ', '_', form.lower())
-	data[pkmn][form]['changes'] = []
+	data[pkmn]['changes'] = []
 	changes = changes.find_next('ul').find_all('li')
 	for change in changes:
-		data[pkmn][form]['changes'].append(change.text)
+		data[pkmn]['changes'].append(change.text)
 
 
 # Pokédex entries
