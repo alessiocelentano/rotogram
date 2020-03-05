84c36e359009d9a1b8255ca00850455427a32696
alessiocelentano <alessiocelentano@outlook.com>
2020-03-05 13:44:16 +0100

    Add set_message function

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ token = open('src/token.txt', 'r').read()
 bot = telebot.TeleBot(token)
 
 
-def find_pkmn(message):
+def find_name(message):
     pkmn = re.sub('/data ', '', message.text)
     pkmn = re.sub('♀', '-f', pkmn)  # For Nidoran♀
     pkmn = re.sub('♂', '-m', pkmn)  # For Nidoran♂
@@ -17,6 +17,35 @@ def find_pkmn(message):
     pkmn = re.sub(' ', '-', pkmn)
     pkmn = re.sub('[^A-Za-z-]', '', pkmn)
     return pkmn
+
+
+def set_message(pkmn_data):
+    base = '''National: {}
+Species: {}
+Catch Rate: {}
+Base Friendship: {}
+Growth Rate: {}
+Egg Cycles: {}
+Artwork: {}
+'''
+    national = pkmn_data['national']
+    species = pkmn_data['species']
+    catch_rate = pkmn_data['catch_rate']
+    base_friendship = pkmn_data['base_friendship']
+    growth_rate = pkmn_data['growth_rate']
+    egg_cycles = pkmn_data['egg_cycles']
+    artwork = pkmn_data['artwork']
+
+    text = base.format(
+        national,
+        species,
+        catch_rate,
+        base_friendship,
+        growth_rate,
+        egg_cycles,
+        artwork
+    )
+    return text
 
 
 @bot.message_handler(commands=['start'])
@@ -28,7 +57,7 @@ def start(message):
 @bot.message_handler(commands=['data'])
 def pkmn_search(message):
     cid = message.chat.id
-    pkmn = find_pkmn(message)
+    pkmn = find_name(message)
     path = 'dist/gen{}/gen{}.json'
 
     for i in range(1, 9):
@@ -41,7 +70,8 @@ def pkmn_search(message):
         bot.send_message(cid, 'Pokémon not found :(')
         return None
 
-    bot.send_message(cid, pkmn_data['national'])
+    text = set_message(pkmn_data)
+    bot.send_message(cid, text)
 
 
 bot.polling(none_stop=True)
